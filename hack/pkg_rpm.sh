04b08aeca28f6ea471397f2dbd1352d1c71f5200
Shiming Zhang <wzshiming@foxmail.com>
2021-11-03 11:26:57 +0800

    Upgrade cri-tools

--- hack/pkg_rpm.sh
+++ hack/pkg_rpm.sh
@@ -4,5 +4,5 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-NOTAR=true kit/pkg_rpm.sh cri-tools amd64,arm64 1.13.0
+NOTAR=true kit/pkg_rpm.sh cri-tools amd64,arm64 1.19.0
 NOTAR=true kit/pkg_rpm.sh kubernetes-cni amd64,arm64 0.8.7
